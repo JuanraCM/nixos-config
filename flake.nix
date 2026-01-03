e53fe23d024625913156a5f9aac75a9f50107567
JuanraCM <juanracm.dev@gmail.com>
2026-01-03 12:26:58 +0000

    feat(flake): migrate inputs to unstable branch

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "My system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:nix-community/stylix/release-25.11";
+      url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

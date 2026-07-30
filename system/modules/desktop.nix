c851a9fdd72bd9223d2c5e2deae4a71ed51b11ac
JuanraCM <juanracm.dev@gmail.com>
2026-07-30 17:47:59 +0200

    fix(niri): libdisplay-info version mismatch

--- system/modules/desktop.nix
+++ system/modules/desktop.nix
@@ -14,11 +14,25 @@
   };
   environment.systemPackages = [ pkgs.xwayland-satellite ];
 
-  programs.niri.enable = true;
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri.override {
+      libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
+        version = "0.3.0";
+        src = pkgs.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
+          owner = "emersion";
+          repo = "libdisplay-info";
+          rev = finalAttrs.version;
+          sha256 = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+        };
+      });
+    };
+  };
 
   services.gvfs.enable = true;
 
-  services.desktopManager.plasma6.enable = true;
+  # services.desktopManager.plasma6.enable = true;
   programs.kdeconnect = {
     enable = true;
   };

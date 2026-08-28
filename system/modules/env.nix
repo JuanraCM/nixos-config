dd75e49b5ab0ed0f8a9d31773d2b6e147a32733f
JuanraCM <juanracm.dev@gmail.com>
2026-08-28 12:13:13 +0100

    feat(npm): workaround for installing global packages

--- system/modules/env.nix
+++ system/modules/env.nix
@@ -3,5 +3,10 @@
     TERMINAL = "ghostty";
     EDITOR = "nvim";
     MANGOHUD = 1;
+
+    # Workaround for installing global NPM packages
+    NPM_CONFIG_PREFIX = "$HOME/.local/share/npm";
+    NPM_CONFIG_CACHE = "$HOME/.cache/npm";
+    PATH = "${NPM_CONFIG_PREFIX}/bin:$PATH";
   };
 }
